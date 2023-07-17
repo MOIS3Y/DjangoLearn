7a8a39e5a2de769fe9639d35127ae38471be8c56
MOIS3Y <stepan@zhukovsky.me>
2023-07-17 20:41:35 +0900

    Part3: Create the public interface – views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/int/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/int/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/int/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
