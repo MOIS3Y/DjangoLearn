28f398c1bf59a13e22756d476453a41ce34c67fb
MOIS3Y <stepan@zhukovsky.me>
2023-07-17 22:30:39 +0900

    Part4: Form processing and cutting down code

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,11 +6,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/int/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/int/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/int/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
